171057f7908d5eea177666d1875e1945aaf459d1
st141152 <alex.xess@yandex.ru>
2025-09-27 15:12:36 +0300

    lesson 1 code upload

---
+++ 2025.09.27_Lesson_1/problem_2497/problem_2497/source.cpp
@@ -0,0 +1,11 @@
+#include <cstdio>
+
+int main(int argc, char** argv)
+{
+	long long a = 0, b = 0, c = 0;
+	scanf_s("%lld", &a);
+	scanf_s("%lld", &b);
+	scanf_s("%lld", &c);
+	printf("%lld\n", a + b + c);
+	return 0;
+}

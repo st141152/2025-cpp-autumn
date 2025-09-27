15c184b3a13bdcc31f210369553687f4c2e5db26
st141152 <alex.xess@yandex.ru>
2025-09-27 11:33:07 +0300

    lesson 1 code upload

---
+++ 2025.09.27_Lesson_1/problem_145/problem_145/source.cpp
@@ -0,0 +1,11 @@
+#include <cstdio>
+
+int main(int argc, char** argv)
+{
+	int a = 0, b = 0, c = 0;
+	scanf_s("%d", &a);
+	scanf_s("%d", &b);
+	scanf_s("%d", &c);
+	printf("%d\n", a * b * c * 2);
+	return 0;
+}

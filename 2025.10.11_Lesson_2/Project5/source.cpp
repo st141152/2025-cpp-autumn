38a073aa1044003103b9f02e5a908422e3da8d90
st141152 <alex.xess@yandex.ru>
2025-10-13 22:33:25 +0300

    lesson 2 code upload

--- 2025.10.11_Lesson_2/Project5/source.cpp
+++ 2025.10.11_Lesson_2/Project5/source.cpp
@@ -7,73 +7,77 @@ int main(int argc, char** argv)
 	int b = 0;
 	int c = 0;
 	scanf_s("%d %d %d", &a, &b, &c);
-
-	// Проверка все ли нули
-	if (a == 0) {
-		if (b == 0) {
-			if (c == 0) {
-				printf("0");
-				return 0;
+	if (a == 0)
+	{
+		if (b == 0)
+		{
+			if (c == 0)
+			{
+				printf("0\n");
 			}
 		}
 	}
-
-	// Вывод постоянного члена a
-	if (a != 0) {
+	if (a != 0)
+	{
 		printf("%d", a);
 	}
 
-	// Вывод члена с x (b)
-	if (b != 0) {
-		// Проверка нужно ли ставить плюс
-		if (b > 0) {
-			if (a != 0) {
+	if (b != 0)
+	{
+		if (b > 0)
+		{
+			if (a != 0)
+			{
 				printf("+");
 			}
 		}
-
-		// Вывод коэффициента и x
-		if (b == -1) {
+		if (b == -1)
+		{
 			printf("-x");
 		}
-		else {
-			if (b == 1) {
+		else
+		{
+			if (b == 1)
+			{
 				printf("x");
 			}
-			else {
+			else
+			{
 				printf("%dx", b);
 			}
 		}
 	}
-
-	// Вывод члена с y (c)
-	if (c != 0) {
-		// Проверка нужно ли ставить плюс
-		if (c > 0) {
-			if (a != 0) {
+	if (c != 0)
+	{
+		if (c > 0)
+		{
+			if (a != 0)
+			{
 				printf("+");
 			}
-			else {
-				if (b != 0) {
+			else
+			{
+				if (b != 0)
+				{
 					printf("+");
 				}
 			}
 		}
-
-		// Вывод коэффициента и y
-		if (c == -1) {
+		if (c == -1)
+		{
 			printf("-y");
 		}
-		else {
-			if (c == 1) {
+		else
+		{
+			if (c == 1)
+			{
 				printf("y");
 			}
-			else {
+			else
+			{
 				printf("%dy", c);
 			}
 		}
 	}
-
-	printf("\n");
 	return 0;
 }

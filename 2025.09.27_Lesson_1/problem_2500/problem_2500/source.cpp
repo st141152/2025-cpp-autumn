6c5019bf9477963a6bc90141f78e4a0f2224df55
st141152 <alex.xess@yandex.ru>
2025-12-07 16:35:08 +0300

    lesson 1 code upload

--- 2025.09.27_Lesson_1/problem_2500/problem_2500/source.cpp
+++ 2025.09.27_Lesson_1/problem_2500/problem_2500/source.cpp
@@ -1,11 +1,12 @@
 #include <cstdio>
 int main(int argc, char** argv)
 {
-	int K = 0;	int N = 0;
-	scanf_s("%d %d", &K, &N);
-	int column = (N - 0.01) / K + 1;
-	int line = (N - 0.01) / K;
-	line = N - line * K;
+	int k = 0;	
+	int n = 0;
+	scanf_s("%d %d", &k, &n);
+	int column = (k - 0.01) / k + 1;
+	int line = (n - 0.01) / k;
+	line = n - line * k;
 	printf("%d %d", column, line);
 	return 0;
 }
